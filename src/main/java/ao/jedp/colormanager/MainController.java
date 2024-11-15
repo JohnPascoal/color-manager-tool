0d6ce82a3933a789b22d9d2ef087271ffc2fa5a0
John Pascoal <johnedgar1@hotmail.com>
2024-11-15 17:40:40 +0100

    Design UI, part 1

---
+++ src/main/java/ao/jedp/colormanager/MainController.java
@@ -0,0 +1,15 @@
+package ao.jedp.colormanager;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class MainController {
+
+    @FXML
+    protected void getColorHexCode(ActionEvent event) {
+
+    }
+}
